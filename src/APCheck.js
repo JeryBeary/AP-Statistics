779a94bba95b07f0496bd53f60333a65e1bc9fb7
Jerry Meng <ymeng@umich.edu>
2015-08-28 23:15:47 -0400

    Fixed the API Requests. Now Responsive.

--- src/APCheck.js
+++ src/APCheck.js
@@ -1,5 +1,5 @@
 
-var API_KEY = "cacad878-8ea2-4b9b-a54d-4fc39a83fa17";
+var API_KEY = "042c618c-50fb-48d9-8ef0-d9879460b899";
 
 
 var oldgames = 0; //total number of games in 5.11
@@ -41,32 +41,32 @@ function drawChart()
         ["Void Staff", 100*(voidGames.oldWins/voidGames.oldGames), 100*(voidGames.newWins/voidGames.newGames) ],
         ["Athene's Unholy Grail", 100*(athenesGames.oldWins/athenesGames.oldGames), 100*(athenesGames.newWins/athenesGames.newGames) ],
         ["Rod of Ages", 100*(rodGames.oldWins/rodGames.oldGames), 100*(rodGames.newWins/rodGames.newGames) ],
-            ]);
+        ]);
 
-    var options = {
+var options = {
 
-      chart: {
-        title: 'Win rates of New and Old AP Items',
-        subtitle: 'Analyzed from a set of normal and ranked games before and after the changes',
-             },
-      textStyle: {
-        color: 'white'
-      },
-      hAxis: {
-            title: 'Percentage',
-            titleTextStyle: {color: 'red', fontSize:'26'},
-            textStyle:{color: '#FFF'},
-            baselineColor:'white',
-        },
-      vAxis: {
-        title: 'Item',
-        textStyle: {color: 'white'},
-        titleTextStyle: {color:'Blue', fontSize:'26'}
-      },
+  chart: {
+    title: 'Win rates of New and Old AP Items',
+    subtitle: 'Analyzed from a set of normal and ranked games before and after the changes',
+},
+textStyle: {
+    color: 'white'
+},
+hAxis: {
+    title: 'Percentage',
+    titleTextStyle: {color: 'red', fontSize:'26'},
+    textStyle:{color: '#FFF'},
+    baselineColor:'white',
+},
+vAxis: {
+    title: 'Item',
+    textStyle: {color: 'white'},
+    titleTextStyle: {color:'Blue', fontSize:'26'}
+},
         bars: 'horizontal', // Required for Material Bar Charts.
         legend: {textStyle: {color:'white'}},
         
-        };
+    };
 
     var chart = new google.charts.Bar(document.getElementById("chart"));
     //convertOptions necessary for Material Bar Charts
@@ -90,33 +90,33 @@ function drawChart2()
         ["Void Staff", 100*(voidGames.oldGames/oldgames), 100*(voidGames.newGames/newgames) ],
         ["Athene's Unholy Grail", 100*(athenesGames.oldGames/oldgames), 100*(athenesGames.newGames/newgames) ],
         ["Rod of Ages", 100*(rodGames.oldGames/oldgames), 100*(rodGames.newGames/newgames) ],
-            ]);
+        ]);
 
-    var options = {
-      chart: {
-        title: 'Pick rates of New and Old AP Items',
-        subtitle: 'Analyzed from a set of normal and ranked games before and after the changes',
-             },
-      textStyle: {
-        color: 'white'
-      },
-      hAxis: {
-            title: 'Percentage',
-            titleTextStyle: {color:'green', fontSize: '26'},
-            textStyle:{color: 'white'},
-            }, 
+var options = {
+  chart: {
+    title: 'Pick rates of New and Old AP Items',
+    subtitle: 'Analyzed from a set of normal and ranked games before and after the changes',
+},
+textStyle: {
+    color: 'white'
+},
+hAxis: {
+    title: 'Percentage',
+    titleTextStyle: {color:'green', fontSize: '26'},
+    textStyle:{color: 'white'},
+}, 
         bars: 'horizontal', // Required for Material Bar Charts.
         legend: {textStyle: {color:'white'}},
-      vAxis:{
-        title: 'Item',
-        titleTextStyle: {color:'yellow', fontSize:'26'},
-        textStyle:{
-            color: 'white'
-        }
-      },
+        vAxis:{
+            title: 'Item',
+            titleTextStyle: {color:'yellow', fontSize:'26'},
+            textStyle:{
+                color: 'white'
+            }
+        },
 
         
-        };
+    };
 
 
     var chart = new google.charts.Bar(document.getElementById("chart_two"));
@@ -129,57 +129,58 @@ function fileSearch()
 {
     //loop through all old games in all files
 
-    //starts off with the Normals in alphabetical order starting with Brazil
-    loadJSON("BR.json" , function response() {
-    // Parse JSON string into object
-    actual = JSON.parse(response);
-    });
-    console.log(actual[0]);
-    checkStat(actual[0].toString(), "br");
+    loadJSON("../gameData/NORMAL_5X5/BR.json",'br');
+    //loop through every JSON file
+    // go through each
     
-    //checkStat()
-
-    //loop through all new games in all files
-    //checkStat()
 }
 
-function loadJSON(file, callback)
+function loadJSON(file, region)
 {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
+    $.getJSON(file, function(data){
+        //data is the json file
+        //there is only a single array
+        var len = data.length;
+                    checkStat(data[1].toString(), region);
+        for(i = 0; i < len; i++){
+
+
+           
+        }
+    });
 }
 
 
 /*
 add to counters
 */
-function checkStat(gameId, region){
+function checkStat(matchId, region){
 
         //HTTP Request to the Riot Games Server using Ajax
         //Uses API key and region as info
         $.ajax({
-            url: 'https://' + region + '.api.pvp.net/api/lol/'+ region +'/v2.2/match/' + gameId + 
-            '?includeTimeline=false&api_key='+ API_KEY,
+            url: 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.2/match/' + matchId + '?&api_key=' + API_KEY,
             //+ SUMMONER_NAME + '?api_key=' + API_KEY,
             type: 'GET',
             dataType: 'json',
-            data: {
 
-            },
-            success: function (json) {
-                var player = json[gameId].participants[0].championId;
-                
+            success: function (json, textStatus, jqXHR) {
+                var item = json.participants[0].stats.item0;
+                for( x = 0; x<10; x++)
+                {
+                    /*
+                    if(json[matchId].participants[0].stats.item0)
+                    {
+                        
+                    }
+                    if(json[matchId].participants[0].stats.item1)
+                    {
 
-                document.getElementById("#items").innerHTML = player;
-                document.getElementById("sID").innerHTML = summonerID;
+                    }
+                    */
+                }
+                document.getElementById("items").innerHTML = item;
+            
 
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -187,7 +188,7 @@ function checkStat(gameId, region){
             }
         });
 
-        }
+    }
 
 
 
